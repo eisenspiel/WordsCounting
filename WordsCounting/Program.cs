16d3fe3393a7ea66a25aed5a2bcfb09f4e21a496
eisenspiel <eisenspiel@gmail.com>
2021-03-11 00:58:16 +0300

    Add project files.

---
+++ WordsCounting/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using WordsCountingLib;
+using System.Reflection;
+
+namespace WordsCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запрашиваем путь к файлу, если путь не введён, по умолчанию путь к файлу в корне диска D:
+            Console.Title = "Words Counting -- Test Task for Digital Design";
+            Console.WriteLine("Please enter the path to your txt file\nor\nJust press Enter if you want to use the default path \"D:\\Vojnaimir.Tom1.utf8.txt\"");
+            string path = Console.ReadLine();
+            if (path == "")
+            {
+                path = @"D:\Vojnaimir.Tom1.UTF8.txt";
+            }
+
+            //Читаем файл  
+            Console.WriteLine($"Trying to open {path}...");
+            string whole_text = "";
+            try
+            {
+                whole_text = System.IO.File.ReadAllText(path);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something wrong with the path you entered. Please start again and enter a valid vath");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"Done! There are {whole_text.Length} symbols.\nNow please wait, the book is big, so it's gonna take a while...");
+
+            //Запускаем один из двух методов
+            Console.WriteLine("Please enter 1 if you want to use private metod with one thread\nor\nEnter anything other for public method with multiple threads");
+            string ChosenMethod = Console.ReadLine();
+            Dictionary<string, int> wordCount = new();
+            if (ChosenMethod == "1")
+            {
+                //Вызываем из библиотеки WordsCountLib приватный метод подсчёта слов из текста          
+                var WordsCountIstance = new WordsCountingClass();
+                var magicMethod = WordsCountIstance.GetType().GetMethod("CountWords", BindingFlags.NonPublic | BindingFlags.Static);
+                wordCount = (Dictionary<string, int>)magicMethod.Invoke(WordsCountIstance, new object[] { whole_text });
+            }
+            else
+            {
+                //Вызываем из библиотеки WordsCountLib публичный метод подсчёта слов из текста с многопоточностью
+                wordCount = WordsCountingClass.CountWordsMultithread(whole_text);
+            }
+
+            // Выводим топ-10 слов
+            Console.WriteLine("Done! Here's top 10 most frequent words from the book:");
+            int topCount = 0;
+            foreach (var item in wordCount)
+            {
+                Console.WriteLine("{0}   {1}", item.Key, item.Value);
+                topCount++;
+                if (topCount == 10)
+                {
+                    break;
+                }
+            }
+
+            //Записываем результат в файл
+            Console.WriteLine("Now please enter the path to the folder where you want to save the results.\nor\nJust press Enter if you want to use the default path \"D:\\\"");
+            path = Console.ReadLine();
+            if (path == "")
+            {
+                path = @"D:\";
+            }
+            Console.WriteLine($"Full results are available in a txt file here: {path}\\results.txt");
+            File.WriteAllLines($"{path}\\results.txt", wordCount.Select(x => x.Key + " " + x.Value));
+
+            // Ждём реакции пользователя
+            Console.WriteLine("Press any key to close the console app");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WordsCountingLib/WordsCountingClass.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Diagnostics;
+
+namespace WordsCountingLib
+{
+    public class WordsCountingClass
+    {
+        private static Dictionary<string, int> CountWords(string TextInput)
+        {
+            //Засекаем время обработки в одном потоке
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            // Заменяем все символы, которые не буквы, на пробелы и переводим текст в нижний регистр
+            TextInput = new Regex("[^a-zA-Zа-яёА-ЯЁ]").Replace(TextInput, " ").ToLower();
+
+            // Создаём массив полученных слов
+            var words = TextInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Создаём словарь для пары "слово - количество"
+            Dictionary<string, int> wordCountUnsorted = new Dictionary<string, int>();
+
+            // Добавляем слово из массива в словарь, если его ещё нет. Если оно уже есть, увеличиваем количество на единицу
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!wordCountUnsorted.ContainsKey(words[i]))
+                {
+                    wordCountUnsorted.Add(words[i], 1);
+                }
+                else
+                {
+                    wordCountUnsorted[words[i]] += 1;
+                }
+            }
+
+            //Сортируем по убыванию вхождений
+            var sortedDict = (from entry in wordCountUnsorted orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            stopWatch.Stop();
+            //выводим время обработки
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine($"The text processing by one thread took {elapsedTime}");
+
+            return (Dictionary<string, int>)sortedDict;
+        }
+
+        public static Dictionary<string, int> CountWordsMultithread(string TextInput)
+        {
+            //Засекаем время обработки в одном потоке
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            // Заменяем все символы, которые не буквы, на пробелы и переводим текст в нижний регистр
+            TextInput = new Regex("[^a-zA-Zа-яёА-ЯЁ]").Replace(TextInput, " ").ToLower();
+
+            // Создаём массив полученных слов
+            var words = TextInput.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Создаём словарь для пары "слово - количество"
+            Dictionary<string, int> wordCountUnsorted = new Dictionary<string, int>();
+
+            // Добавляем слово из массива в словарь, если его ещё нет. Если оно уже есть, увеличиваем количество на единицу
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!wordCountUnsorted.ContainsKey(words[i]))
+                {
+                    wordCountUnsorted.Add(words[i], 1);
+                }
+                else
+                {
+                    wordCountUnsorted[words[i]] += 1;
+                }
+            }
+
+            /*Такой способ выдаёт ошибку -- потоки рвут словарь на части, поэтому не будем его параллелить
+            Parallel.For(0, words.Length, i => {
+                if (!wordCountUnsorted.ContainsKey(words[i]))
+                {
+                    wordCountUnsorted.Add(words[i], 1);
+                }
+                else
+                {
+                    wordCountUnsorted[words[i]] += 1;
+                }                
+            });
+            */
+
+            //Сортируем по убыванию вхождений
+            var sortedDict = (from entry in wordCountUnsorted.AsParallel() orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            stopWatch.Stop();
+            //выводим время обработки
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds);
+            Console.WriteLine($"The text processing by multiple threads in a public metod took {elapsedTime}");
+
+            return (Dictionary<string, int>)sortedDict;
+        }
+    }
+}
